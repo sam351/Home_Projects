c7d66924655a68799043377e9c2b1a1f8505608d
sam <ksmo9191@hanmail.net>
2020-01-26 22:01:44 +0900

    Modify balance_checker.py

--- balance_checker/balance_checker.py
+++ balance_checker/balance_checker.py
@@ -12,21 +12,27 @@ import numpy as np
 
 
 # 1. preprocessing report statement xlsx
-system_df = pd.read_excel('./결의서전표.xlsx')
-system_df = system_df.iloc[ :-1][['결의일자-번호','수입금액','지출금액']]
+system_df = pd.read_excel('./결의서전표.xlsx')[['결의일자-번호','수입금액','지출금액']]
 system_df['결의일자'] = [ string[:-2] for string in system_df['결의일자-번호'] ]
 system_df = system_df.drop('결의일자-번호', axis=1)
 system_df = system_df.set_index('결의일자')
-# system_df
+system_df.index = pd.to_datetime(system_df.index).date
+system_df = system_df.sort_index()
+
+print(system_df.shape)
+system_df.head()
 
 
 # In[3]:
 
 
 # 2. preprocessing account xls
-account_df = pd.read_excel('./통장거래내역.xls', skiprows = 9)[['거래일자','입금금액(원)','출금금액(원)']]
+account_df = pd.read_excel('./통장거래내역.xls', skiprows = 0)[['거래일자','입금금액(원)','출금금액(원)']]
 account_df = account_df.set_index('거래일자').fillna(0).astype(int)
-# account_df
+account_df.index = pd.to_datetime(account_df.index).date
+
+print(account_df.shape)
+account_df.tail()
 
 
 # In[4]:
@@ -45,7 +51,7 @@ print('>>> 수입총액은 정상입니다.\n') if system_income == account_inco
 system_spending = sum(system_df.지출금액)
 account_spending = sum(account_df['출금금액(원)'])
 print(f'(2) 결의서전표 지출총액 : { system_spending }원, 통장거래내역 출금총액 : { account_spending }원')
-print('>>> 지출총액은 정상입니다.\n') if system_income == account_income else print('>>> 경고!!! 지출총액이 다릅니다!\n')
+print('>>> 지출총액은 정상입니다.\n') if system_spending == account_spending else print('>>> 경고!!! 지출총액이 다릅니다!\n')
 
 print('='*70, end='\n'*2)
 
@@ -54,24 +60,20 @@ print('='*70, end='\n'*2)
 
 
 # 4. migrating rows with same date - calcaulating total amount of each date
-system_df_groupby = system_df.groupby('결의일자')[['수입금액','지출금액']].sum()
+system_df_groupby = system_df.groupby(system_df.index)[['수입금액','지출금액']].sum()
 # system_df_groupby
 
-account_df_groupby = account_df.groupby('거래일자')[['입금금액(원)','출금금액(원)']].sum()
+account_df_groupby = account_df.groupby(account_df.index)[['입금금액(원)','출금금액(원)']].sum()
 # account_df_groupby
-
-
-# In[6]:
-
 
 # 5. merging two df
 merge_df = system_df_groupby.merge(account_df_groupby, how='outer',left_on=system_df_groupby.index, right_on=account_df_groupby.index)
 merge_df = merge_df.rename(columns={'key_0':'날짜'}).set_index('날짜')
 merge_df = merge_df.fillna(0).astype(int)
-# merge_df
+merge_df.head()
 
 
-# In[9]:
+# In[6]:
 
 
 # 6. comparing incomes of each date
@@ -109,7 +111,7 @@ else:
 print('='*70, end='\n'*2)
 
 
-# In[8]:
+# In[7]:
 
 
 end_input = input('종료하시려면 아무 키나 입력하세요 >>> ')
